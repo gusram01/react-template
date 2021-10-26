d187a425ed74843b9195f36042aeb2d6d9dc64bb
Gus Ramírez <gustavo@coconutt.io>
2021-10-25 22:23:43 -0500

    build: change from standard to airbnb; add jest config

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,8 +2,15 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
+    node: true,
   },
-  extends: ['plugin:react/recommended', 'standard', 'prettier'],
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+    'plugin:react/jsx-runtime',
+    'airbnb-typescript',
+    'prettier',
+  ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: {
@@ -11,10 +18,12 @@ module.exports = {
     },
     ecmaVersion: 13,
     sourceType: 'module',
+    project: './tsconfig.eslint.json',
   },
   plugins: ['react', '@typescript-eslint', 'prettier'],
   rules: {
     'prettier/prettier': 'warn',
+    'react/jsx-filename-extension': [1, { extensions: ['.tsx', '.jsx'] }],
     'no-unused-vars': 'warn',
   },
 };
